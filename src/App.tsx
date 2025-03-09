8276e771f41976500d7e153cf7eacd72716035be
Koluch Aleksander <aleksander.koluch@coig.pl>
2025-03-09 18:12:06 +0100

    Added server and init code in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,44 +10,6 @@ export const FetchRecentGames = async (steamId: string) => {
 
 function App() {
 
-  //   fetchSteamData()
-  //     .then((userRecentlyPlayedGames) => {
-        
-  //       if (userRecentlyPlayedGames?.response?.games) {
-  //         const fetchedGames = userRecentlyPlayedGames.response.games;
-  //         setGames(fetchedGames);
-  //         fetchedGames.forEach((game: SteamGame) => {
-  //           fetchLatestReview(game.appid)
-  //             .then(({latestReview, score}) => {
-  //               if (latestReview) {
-  //                 setReviews((prevReviews) => ({
-  //                   ...prevReviews,
-  //                   [game.appid]: latestReview,
-  //                 }));
-  //               }
-  //               if (score) {
-  //                 setScore((prevScore) => ({
-  //                   ...prevScore,
-  //                   [game.appid]: score,
-  //                 }));
-  //               }
-  //             })
-  //           fetchIMG(game.appid)
-  //             .then((image: string) => {
-  //               if (image) {
-  //                 setImage((prevReviews) => ({
-  //                   ...prevReviews,
-  //                   [game.appid]: image,
-  //                 }));
-  //               }
-  //             })
-  //         });
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       console.error('Error fetching Steam data:', error);
-  //     });
-  // }, []);
 
   return (
     <div className="App">
@@ -59,3 +21,41 @@ function App() {
 }
 
 export default App;
+
+//Init code
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
