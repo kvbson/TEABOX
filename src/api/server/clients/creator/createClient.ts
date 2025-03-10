09c86a1eefd98c69c5d4e5f2175069568a5ebf88
kvbson <jakub.jackob@gmail.com>
2025-03-10 20:01:42 +0100

    fix

---
+++ src/api/server/clients/creator/createClient.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosInstance } from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const createApiClient = (baseURL: string): AxiosInstance => {
+  const api = axios.create({
+    baseURL,
+    timeout: 5000,
+    headers: {
+      'Content-Type': 'application/json',
+      'User-Agent': 'Teabox/0.0.0',
+    },
+    params: {
+      key: process.env.VITE_STEAM_API_KEY,
+    },
+  });
+
+  api.interceptors.request.use(config => {
+    console.log(`Making request to ${config.url}`);
+    return config;
+  });
+
+  api.interceptors.response.use(response => {
+    console.log(`[Success] ${response.status} ${response.config.url}`);
+    return response;
+  });
+
+  return api;
+};
--- src/api/server/clients/steamStoreApiClient.ts
+++ src/api/server/clients/steamStoreApiClient.ts
@@ -1,30 +1,5 @@
-import axios from 'axios';
-import dotenv from 'dotenv';
+import { createApiClient } from "./creator/createClient";
 
-dotenv.config();
-
-const steamStoreApi = axios.create({
-  baseURL: 'https://store.steampowered.com/api/',
-  timeout: 5000,
-  headers: {
-    'Content-Type': 'application/json',
-    'User-Agent': 'Teabox/0.0.0'
-  },
-  params: {
-    key: process.env.VITE_STEAM_API_KEY
-  }
-});
-
-steamStoreApi.interceptors.request.use(config => {
-  console.log(`Making request to ${config.url}`);
-  return config;
-});
-
-steamStoreApi.interceptors.response.use(
-  response => {
-    console.log(`[Success] ${response.status} ${response.config.url}`);
-    return response;
-  },
-);
+const steamStoreApi = createApiClient('https://store.steampowered.com/api/');
 
 export default steamStoreApi;
