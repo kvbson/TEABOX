83dce86d8dff48715cb1d7d894b076792dd5dbc4
kvbson <jakub.jackob@gmail.com>
2025-05-30 18:12:19 +0200

    fixed import css

--- src/web/main.tsx
+++ src/web/main.tsx
@@ -1,11 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './css/header.css';
-import './css/index.css';
-import './css/loadingOverlay.css';
-import './css/recentGames.css';
-import './css/sidebar.css';
-import './css/pages/Prefferences.css';
+//added to import all css globally in web vite environment
+import.meta.glob('./css/**/*.css', { eager: true });
 
 createRoot(document.getElementById('root')!).render(
   <App />,
