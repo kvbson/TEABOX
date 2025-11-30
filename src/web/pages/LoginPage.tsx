f9414f69b3bc5dd03eebd533603c0e18479bd79b
PMilczarek64 <mammoth1907@gmail.com>
2025-11-30 21:20:18 +0100

    Add MUI Theme

--- src/web/pages/LoginPage.tsx
+++ src/web/pages/LoginPage.tsx
@@ -1,31 +1,94 @@
-interface LoginPageProps {
+import React, { useState } from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Paper,
+  TextField,
+  Typography
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+
+export interface LoginPageProps {
   onLogin: () => void;
 }
 
 const LoginPage: React.FC<LoginPageProps> = ({ onLogin }) => {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        flexDirection: 'column',
+    <Container
+      maxWidth="xs"
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
       }}
     >
-      <div>
-        <h3>
-          LOGIN: <input type="text" />
-        </h3>
-      </div>
-      <div>
-        <h3>
-          HASŁO: <input type="password" />
-        </h3>
-        {/* tutaj wywołujemy prop onLogin */}
-        <button onClick={onLogin}>LOGIN</button>
-      </div>
-    </div>
+      <Paper elevation={6} sx={{ p: 4, width: "100%", borderRadius: 3 }}>
+
+        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+
+          <Typography component="h1" variant="h5" fontWeight={600}>
+            Logowanie
+          </Typography>
+        </Box>
+
+        {/* FORM */}
+        <Box sx={{ mt: 3, display: "flex", flexDirection: "column", gap: 2 }}>
+          <TextField
+            variant="outlined"
+            placeholder="Email"
+            sx={{
+              '& .MuiOutlinedInput-input::placeholder': {
+                color: 'var(textPrimary)',
+                opacity: 1,
+              },
+              "& .MuiOutlinedInput-root": {
+                "& fieldset, & .MuiOutlinedInput": {
+                  border: "1px solid",
+                },
+              },
+            }}
+          />
+          <TextField
+            type="password"
+            placeholder="Password"
+            fullWidth
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+               sx={{
+              '& .MuiOutlinedInput-input::placeholder': {
+                color: 'var(textPrimary)',
+                opacity: 1,
+              },
+              "& .MuiOutlinedInput-root": {
+                "& fieldset, & .MuiOutlinedInput": {
+                  border: "1px solid",
+                },
+              },
+            }}
+          />
+
+          <Button
+            variant="contained"
+            size="large"
+            fullWidth
+            sx={{ mt: 1 }}
+            onClick={onLogin}    // 🔥 zachowujemy Twoją logikę
+          >
+            Zaloguj się
+          </Button>
+        </Box>
+
+      </Paper>
+    </Container>
   );
 };
 
